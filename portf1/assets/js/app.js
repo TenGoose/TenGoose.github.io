af72fdcc747be23f6b718b62da8273053ac6b1b8
TenGoose <82271156+TenGoose@users.noreply.github.com>
2023-02-07 11:23:24 +0700

    Prettify JS

--- portf1/assets/js/app.js
+++ portf1/assets/js/app.js
@@ -1,81 +1,67 @@
-$(function() {
+$(function () {
+	let header = $("#header"),
+		introH = $("#intro").innerHeight(),
+		scrollOffset = $(window).scrollTop();
 
-    let header = $("#header"), 
-        introH = $("#intro").innerHeight(),
-        scrollOffset = $(window).scrollTop();
+	/* Fixed header */
+	checkScroll(scrollOffset);
 
-    
-    /* Fixed header */
-    checkScroll(scrollOffset);
+	$(window).on("scroll", function () {
+		scrollOffset = $(this).scrollTop();
 
-    $(window).on("scroll", function() {
+		checkScroll(scrollOffset);
+	});
 
-        scrollOffset = $(this).scrollTop();
+	function checkScroll() {
+		if (scrollOffset >= introH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
 
-        checkScroll(scrollOffset);
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
 
+		let $this = $(this),
+			blockId = $(this).data("scroll"),
+			blockOffset = $(blockId).offset().top;
 
-    });
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
 
-    function checkScroll() {
+		$("html, body").animate(
+			{
+				scrollTop: blockOffset,
+			},
+			500
+		);
+	});
 
-        if( scrollOffset >= introH ) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
-    }
+	/* Menu nav toggle*/
+	$("#nav_toggle").on("click", function (event) {
+		event.preventDefault();
 
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
 
-    /* Smooth scroll */
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
+	/* Collapse */
+	$("[data-collapse]").on("click", function (event) {
+		event.preventDefault();
 
-        let $this = $(this),
-            blockId = $(this).data('scroll'),
-            blockOffset = $(blockId).offset().top;
+		let $this = $(this),
+			blockId = $(this).data("collapse");
 
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
+		$this.toggleClass("active");
+	});
 
-
-        $("html, body").animate({
-            scrollTop: blockOffset
-        }, 500);
-    });
-
-
-    /* Menu nav toggle*/
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
-    });
-
-
-    /* Collapse */
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        let $this = $(this),
-            blockId = $(this).data('collapse');
-        
-        $this.toggleClass("active")
-        
-
-
-    });
-
-    /* Slider */
-    $("[data-slider]").slick({
-        Infinity: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
-
+	/* Slider */
+	$("[data-slider]").slick({
+		Infinity: true,
+		fade: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
 });
-
-
